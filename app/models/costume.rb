f315c8132293ba5c289c6e9cb76bae160887cd67
Naz Khan <nazmuskhan@yahoo.com>
2017-03-24 17:48:21 -0400

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,4 @@
+class Costume < ActiveRecord::Base
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+end
--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,17 @@
+class CreateCostumeStores < ActiveRecord::Migration
+
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :num_of_stores
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+      t.integer :costume_inventory
+    end
+  end
+
+end
 # Create your costume_stores migration here
